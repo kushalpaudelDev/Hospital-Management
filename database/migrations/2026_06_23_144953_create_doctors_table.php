8137b6d7473e0e223fac11c32efd61e6fbd52bd3
kushalpaudelDev <kushalpaudel589@gmail.com>
2026-06-23 21:00:38 +0545

    updaates im migrations afteraartisian

--- database/migrations/2026_06_23_144953_create_doctors_table.php
+++ database/migrations/2026_06_23_144953_create_doctors_table.php
@@ -6,20 +6,24 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('doctors', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->foreignId('department_id')->constrained();
+            $table->string('specialization');
+            $table->string('license_number')->unique();
+            $table->text('qualification');
+            $table->integer('experience_years')->default(0);
+            $table->decimal('consultation_fee', 10, 2)->default(0);
+            $table->string('phone');
+            $table->text('address')->nullable();
+            $table->boolean('is_available')->default(true);
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('doctors');
--- database/migrations/2026_06_23_145025_create_appointments_table.php
+++ database/migrations/2026_06_23_145025_create_appointments_table.php
@@ -13,6 +13,16 @@ return new class extends Migration
     {
         Schema::create('appointments', function (Blueprint $table) {
             $table->id();
+             $table->string('appointment_number')->unique();
+            $table->foreignId('patient_id')->constrained();
+            $table->foreignId('doctor_id')->constrained();
+            $table->date('appointment_date');
+            $table->time('appointment_time');
+            $table->enum('type', ['consultation', 'follow_up', 'emergency'])->default('consultation');
+            $table->text('symptoms')->nullable();
+            $table->text('notes')->nullable();
+            $table->enum('status', ['scheduled', 'confirmed', 'completed', 'cancelled', 'no_show'])->default('scheduled');
+            $table->foreignId('created_by')->constrained('users');
             $table->timestamps();
         });
     }

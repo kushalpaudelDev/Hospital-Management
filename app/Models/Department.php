939c9961f77357e283f418401a80124e0103ab2f
kushalpaudelDev <kushalpaudel589@gmail.com>
2026-06-24 08:58:59 +0545

    create department model n define doctors association

--- app/Models/Department.php
+++ app/Models/Department.php
@@ -3,8 +3,14 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
 
 class Department extends Model
 {
-    //
+    protected $fillable = ['name', 'code', 'description', 'phone', 'is_active'];
+        public function doctors(): HasMany
+{
+    return $this->hasMany(Doctor::class);
+}
 }

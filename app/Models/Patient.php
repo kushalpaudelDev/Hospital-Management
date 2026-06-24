8ceb655bac7865354af05a33a91e50765218d3f9
kushalpaudelDev <kushalpaudel589@gmail.com>
2026-06-24 16:55:40 +0545

    update patient model

--- app/Models/Patient.php
+++ app/Models/Patient.php
@@ -6,5 +6,44 @@ use Illuminate\Database\Eloquent\Model;
 
 class Patient extends Model
 {
-    //
+    protected $fillable = [
+        'patient_id', 'first_name', 'last_name', 'email', 'phone',
+        'date_of_birth', 'gender', 'blood_group', 'address',
+        'emergency_contact_name', 'emergency_contact_phone',
+        'allergies', 'chronic_diseases', 'status'
+    ];
+
+    protected $casts = [
+        'date_of_birth' => 'date',
+    ];
+
+    public function appointments(): HasMany
+    {
+        return $this->hasMany(Appointment::class);
+    }
+
+    public function medicalRecords(): HasMany
+    {
+        return $this->hasMany(MedicalRecord::class);
+    }
+
+    public function prescriptions(): HasMany
+    {
+        return $this->hasMany(Prescription::class);
+    }
+
+    public function bills(): HasMany
+    {
+        return $this->hasMany(Bill::class);
+    }
+
+    public function medicalHistories(): HasMany
+    {
+        return $this->hasMany(PatientMedicalHistory::class);
+    }
+
+    public function getFullNameAttribute(): string
+    {
+        return "{$this->first_name} {$this->last_name}";
+    }
 }

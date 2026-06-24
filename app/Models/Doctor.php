2f72d3fded0a1decb4d9f99829f8489a627c54fb
kushalpaudelDev <kushalpaudel589@gmail.com>
2026-06-24 16:51:30 +0545

    Update Doctor model

--- app/Models/Doctor.php
+++ app/Models/Doctor.php
@@ -3,8 +3,39 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Doctor extends Model
 {
-    //
+        protected $fillable = [
+        'user_id', 'department_id', 'specialization', 'license_number',
+        'qualification', 'experience_years', 'consultation_fee',
+        'phone', 'address', 'is_available'
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function department(): BelongsTo
+    {
+        return $this->belongsTo(Department::class);
+    }
+
+    public function appointments(): HasMany
+    {
+        return $this->hasMany(Appointment::class);
+    }
+
+    public function medicalRecords(): HasMany
+    {
+        return $this->hasMany(MedicalRecord::class);
+    }
+
+    public function prescriptions(): HasMany
+    {
+        return $this->hasMany(Prescription::class);
+    }
 }

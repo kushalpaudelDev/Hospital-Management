4e9fa584230147bf9fb4e852a0709f86760c00a9
kushalpaudelDev <kushalpaudel589@gmail.com>
2026-06-24 19:54:28 +0545

    updates in use

--- app/Models/Prescription.php
+++ app/Models/Prescription.php
@@ -3,8 +3,31 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Prescription extends Model
 {
-    //
+    protected $fillable = [
+        'medical_record_id', 'doctor_id', 'patient_id',
+        'instructions', 'prescribed_date'
+    ];
+
+    protected $casts = [
+        'prescribed_date' => 'date',
+    ];
+
+    public function medicalRecord(): BelongsTo
+    {
+        return $this->belongsTo(MedicalRecord::class);
+    }
+
+    public function doctor(): BelongsTo
+    {
+        return $this->belongsTo(Doctor::class);
+    }
+
+    public function patient(): BelongsTo
+    {
+        return $this->belongsTo(Patient::class);
+    }
 }

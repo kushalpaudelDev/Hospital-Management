4365472d507f091b0a3e1d703323db26746ccc38
kushalpaudelDev <kushalpaudel589@gmail.com>
2026-06-25 08:00:46 +0545

    updates in seeders

--- database/seeders/MedicineCategorySeeder.php
+++ database/seeders/MedicineCategorySeeder.php
@@ -2,16 +2,23 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\MedicineCategory;
 use Illuminate\Database\Seeder;
 
 class MedicineCategorySeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
-        //
+        $categories = [
+            ['name' => 'Antibiotics', 'code' => 'ANTB', 'description' => 'Infection fighting medicines'],
+            ['name' => 'Pain Relief', 'code' => 'PAIN', 'description' => 'Pain killers and analgesics'],
+            ['name' => 'Vitamins', 'code' => 'VITA', 'description' => 'Vitamin supplements'],
+            ['name' => 'Cardiac', 'code' => 'CARD', 'description' => 'Heart medicines'],
+            ['name' => 'Diabetes', 'code' => 'DIAB', 'description' => 'Diabetes management'],
+        ];
+
+        foreach ($categories as $cat) {
+            MedicineCategory::create($cat);
+        }
     }
 }

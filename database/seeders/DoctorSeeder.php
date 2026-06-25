ebd9d4fe74466eded976281527542f970f3bebf2
kushalpaudelDev <kushalpaudel589@gmail.com>
2026-06-25 07:59:35 +0545

    updates in seeders

--- database/seeders/DoctorSeeder.php
+++ database/seeders/DoctorSeeder.php
@@ -2,16 +2,56 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Doctor;
+use App\Models\User;
+use App\Models\Department;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DoctorSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
-        //
+        // Create users for doctors first
+        $doctorUsers = [
+            [
+                'name' => 'Dr. Prakash Joshi',
+                'email' => 'dr.prakash@hospital.com',
+                'password' => Hash::make('password'),
+            ],
+            [
+                'name' => 'Dr. Anjali Karki',
+                'email' => 'dr.anjali@hospital.com',
+                'password' => Hash::make('password'),
+            ],
+            [
+                'name' => 'Dr. Binod Thapa',
+                'email' => 'dr.binod@hospital.com',
+                'password' => Hash::make('password'),
+            ],
+        ];
+
+        $departments = Department::all();
+
+        foreach ($doctorUsers as $index => $userData) {
+            $user = User::create($userData);
+
+            Doctor::create([
+                'user_id' => $user->id,
+                'department_id' => $departments[$index]->id,
+                'specialization' => match($index) {
+                    0 => 'Cardiologist',
+                    1 => 'Neurologist',
+                    2 => 'Orthopedic Surgeon',
+                },
+                'license_number' => 'NMC-' . (1000 + $index),
+                'qualification' => 'MBBS, MD',
+                'experience_years' => 10 + $index * 2,
+                'consultation_fee' => 500 + ($index * 200),
+                'phone' => '981000000' . ($index + 1),
+                'address' => 'Kathmandu, Nepal',
+                'is_available' => true,
+            ]);
+        }
     }
 }

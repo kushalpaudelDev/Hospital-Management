33a0472a226237826a1c8f0452210f5c5da1e538
kushalpaudelDev <kushalpaudel589@gmail.com>
2026-06-25 09:05:19 +0545

    updates in payment

--- app/Models/Payment.php
+++ app/Models/Payment.php
@@ -6,5 +6,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class Payment extends Model
 {
-    //
+     protected $fillable = [
+        'bill_id', 'amount', 'method',
+        'transaction_id', 'payment_date', 'notes'
+    ];
+
+    protected $casts = [
+        'payment_date' => 'date',
+        'amount' => 'decimal:2',
+    ];
+
+    public function bill(): BelongsTo
+    {
+        return $this->belongsTo(Bill::class);
+    }
 }

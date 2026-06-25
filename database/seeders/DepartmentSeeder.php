631a717742b5882ddd393da381f5c063af9c7ef9
kushalpaudelDev <kushalpaudel589@gmail.com>
2026-06-25 07:58:40 +0545

    updates iin seeders

--- database/seeders/DepartmentSeeder.php
+++ database/seeders/DepartmentSeeder.php
@@ -1,17 +1,29 @@
 <?php
 
 namespace Database\Seeders;
+use App\Models\Department;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DepartmentSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
+    
     public function run(): void
     {
-        //
+        $departments = [
+            ['name' => 'Cardiology', 'code' => 'CARD', 'description' => 'Heart and cardiovascular care'],
+            ['name' => 'Neurology', 'code' => 'NEUR', 'description' => 'Brain and nervous system'],
+            ['name' => 'Orthopedics', 'code' => 'ORTH', 'description' => 'Bones and joints'],
+            ['name' => 'Pediatrics', 'code' => 'PEDI', 'description' => 'Children healthcare'],
+            ['name' => 'General Medicine', 'code' => 'GENM', 'description' => 'General health checkups'],
+            ['name' => 'Emergency', 'code' => 'EMER', 'description' => 'Emergency and trauma care'],
+            ['name' => 'Dermatology', 'code' => 'DERM', 'description' => 'Skin care'],
+            ['name' => 'Ophthalmology', 'code' => 'OPHT', 'description' => 'Eye care'],
+        ];
+
+        foreach ($departments as $dept) {
+            Department::create($dept);
+        }
     }
-}
+    }
+

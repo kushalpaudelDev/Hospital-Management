604bca236623bd21208648e28579e35ffc5adf3a
kushalpaudelDev <kushalpaudel589@gmail.com>
2026-06-26 09:17:23 +0545

    update in medicinemedicalcategory

--- app/Models/MedicineCategory.php
+++ app/Models/MedicineCategory.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class MedicineCategory extends Model
 {
-    //
+     protected $fillable = ['name', 'code', 'description', 'is_active'];
+
+    public function medicines(): HasMany
+    {
+        return $this->hasMany(Medicine::class, 'category_id');
+    }
 }

1a1d2ba911921493ae42a9622906d072e8a80e0d
kushalpaudelDev <kushalpaudel589@gmail.com>
2026-06-28 10:46:53 +0545

    update models dispensemedicine model

--- app/Models/DispensedMedicine.php
+++ app/Models/DispensedMedicine.php
@@ -3,8 +3,25 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class DispensedMedicine extends Model
 {
-    //
+    protected $fillable = [
+        'prescription_id', 'medicine_id', 'quantity', 'dispensed_at',
+    ];
+
+    protected $casts = [
+        'dispensed_at' => 'datetime',
+    ];
+
+    public function prescription(): BelongsTo
+    {
+        return $this->belongsTo(Prescription::class);
+    }
+
+    public function medicine(): BelongsTo
+    {
+        return $this->belongsTo(Medicine::class);
+    }
 }

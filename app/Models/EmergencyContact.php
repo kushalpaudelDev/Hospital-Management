d3e6eb0bddaa80bd5dc7ed99b242e79cd9a28873
kushalpaudelDev <kushalpaudel589@gmail.com>
2026-06-28 10:46:53 +0545

    update models emergencycontact model

--- app/Models/EmergencyContact.php
+++ app/Models/EmergencyContact.php
@@ -3,8 +3,16 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class EmergencyContact extends Model
 {
-    //
+    protected $fillable = [
+        'patient_id', 'name', 'relationship', 'phone',
+    ];
+
+    public function patient(): BelongsTo
+    {
+        return $this->belongsTo(Patient::class);
+    }
 }
